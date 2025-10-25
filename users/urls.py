69ad3f1f5bc5a7001fdb47af7315dd31ed522706
bossmoon229 <alinaignatich@gmail.com>
2025-10-25 14:39:20 +0300

    add registration

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
    path("login/", views.login_user, name="login_user"),
-   path("logout_user", views.logout_user, name="logout_user")
+   path("logout_user", views.logout_user, name="logout_user"),
+   path("register_user/", views.register_user, name="register_user"),
 ]
